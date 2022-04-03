386c27286524b670c41e5056a27dccf1e0e3d3ec
Caleb Dauphin <calebdauphindev@gmail.com>
2022-04-03 07:41:20 -0400

    clean app.js, add searchform.js file to build form

--- react-dictionary/src/App.js
+++ react-dictionary/src/App.js
@@ -1,10 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
+import SearchForm from "./SearchForm"
 
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'>hello</header>
+			<div className='container'>
+				<header className='App-header'>hello</header>
+				<main>
+					<SearchForm />
+				</main>
+				<footer className='text-center'>coded by Cassandra Dauphin</footer>
+			</div>
 		</div>
 	)
 }
