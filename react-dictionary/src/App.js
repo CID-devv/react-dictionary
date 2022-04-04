ef1cce023dbabff95a31238de90cd93b70e71bc4
Caleb Dauphin <calebdauphindev@gmail.com>
2022-04-04 11:48:14 -0400

    start on form

--- react-dictionary/src/App.js
+++ react-dictionary/src/App.js
@@ -6,7 +6,7 @@ function App() {
 	return (
 		<div className='App'>
 			<div className='container'>
-				<header className='App-header'></header>
+				<header className='App-header'>React Dictionary App</header>
 				<main>
 					<SearchForm />
 				</main>
--- react-dictionary/src/SearchForm.js
+++ react-dictionary/src/SearchForm.js
@@ -1,17 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
 import axios from "axios"
 import "./SearchForm.css"
 
-export default function Searchform() {
+export default function SearchForm() {
+	let [keyword, setKeyword] = useState("")
+
 	function search(e) {
 		e.preventDefault()
 		alert("searching")
 	}
 
+	function handleKeywordChange(e) {
+		console.log(e)
+	}
+
 	return (
-		<div className='searchForm'>
+		<div className='SearchForm'>
 			<form onSubmit={search}>
-				<input type='search' />
+				<input type='search' onChange={handleKeywordChange} />
 			</form>
 		</div>
 	)
